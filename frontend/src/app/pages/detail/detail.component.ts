d7aaf6592b316648bd9d7195137bfcf7da0daf3a
AggiePaola <104395719+AggiePaola@users.noreply.github.com>
2025-08-24 16:38:48 -0500

    reviews

--- frontend/src/app/pages/detail/detail.component.ts
+++ frontend/src/app/pages/detail/detail.component.ts
@@ -1,50 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { DeviceService } from '../../services/device.service';
+import { FormsModule } from '@angular/forms';
+import { Device, DeviceService } from '../../services/device.service';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
-  device: any;
-  ;
-
-  getImageUrl(device: any): string {
-    if (!device || !device.imagen) {
-      return '/imagenes/default.png'; // opcional: imagen por defecto
-    }
-    return device.imagen.startsWith('http')
-      ? device.imagen
-      : 'public/imagenes/' + device.imagen;
-  }
+  device: Device | undefined;
   isModalOpen = false;
+  newReview = '';
+
   constructor(
     private route: ActivatedRoute,
     private deviceService: DeviceService
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     const idParam = this.route.snapshot.paramMap.get('id');
+    console.log('idParam =', idParam);  // <--- revisa que no sea null
     if (idParam) {
       const id = Number(idParam);
       this.deviceService.getDeviceById(id).subscribe({
-        next: data => this.device = data,
+        next: data => {
+          console.log('device recibido =', data);  // <--- revisa la consola
+          this.device = data;
+        },
         error: err => console.error('Error al cargar el dispositivo:', err)
       });
     }
+  }
+
+
+  getImageUrl(): string {
+    if (!this.device || !this.device.imagen) return 'assets/imagenes/default.png';
+    return this.device.imagen.startsWith('http')
+      ? this.device.imagen
+      : 'assets/imagenes/' + this.device.imagen;
   }
 
   openModal() {
     this.isModalOpen = true;
   }
 
-  // Cerrar modal
   closeModal() {
     this.isModalOpen = false;
+    this.newReview = '';
   }
 }
+
