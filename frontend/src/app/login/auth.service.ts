eb47007e87c00ae101404f95eafb4db8c14d10a1
AggiePaola <104395719+AggiePaola@users.noreply.github.com>
2025-09-14 18:52:16 -0500

    prueba php 1

--- frontend/src/app/login/auth.service.ts
+++ frontend/src/app/login/auth.service.ts
@@ -9,18 +9,14 @@ export class AuthService {
   private apiUrl = 'http://localhost/login-api/login.php';
 
   constructor(private http: HttpClient) { }
-
   login(datos: { usuario: string, contrasena: string }): Observable<any> {
-    return this.http.post<any>(this.apiUrl, datos);
+    return this.http.post<any>('http://localhost/login-api/login.php', datos);
   }
 
-
-  register(email: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/register.php`, {
-      Usuario: email,
-      Contrasena: password
-    });
+  register(usuario: string, contrasena: string): Observable<any> {
+    return this.http.post<any>('http://localhost/login-api/register.php', { usuario, contrasena });
   }
+
 }
 
 
