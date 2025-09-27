439620571d78745126ff3cc5931c0aa47f39febc
AggiePaola <104395719+AggiePaola@users.noreply.github.com>
2025-09-27 13:07:22 -0500

    hora

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginModalComponent } from '../../login/login.modal';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-header',
+  standalone: true,
   templateUrl: './header.component.html',
   imports: [LoginModalComponent],
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent { }
+export class HeaderComponent implements OnInit {
+  horaActual: string | undefined;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.cargarHora();
+    setInterval(() => this.cargarHora(), 60000);
+  }
+
+  cargarHora() {
+    this.http.get<any>('https://worldtimeapi.org/api/timezone/America/Bogota')
+      .subscribe(data => {
+        console.log('Respuesta API:', data); // 👀 Ver en consola
+        this.horaActual = new Date(data.datetime).toLocaleTimeString();
+      });
+  }
+
+}
